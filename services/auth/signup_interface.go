1e86d3e19734b8a7b358a74612fc05f7f04b6336
ninsid711 <siddy0041@gmail.com>
2024-06-22 18:29:54 +0530

    changes to factory func

--- services/auth/signup_interface.go
+++ services/auth/signup_interface.go
@@ -1,6 +1,24 @@
+// package auth
+
+// import (
+// 	"Phase-R-Backend/db/models"
+
+// 	"gofr.dev/pkg/gofr"
+// )
+
+// type User interface {
+// 	CreateUser(ctx *gofr.Context, model *models.User) (*models.User, error)
+// 	FetchUser(ctx *gofr.Context, UUID string) (*models.User, error)
+// }
+
 package auth
 
 import (
 	"gofr.dev/pkg/gofr"
-	"Phase-R-Backend/db/models"
+	"github.com/Phase-R/Phase-R-Backend/db/models"
 )
+
+type User interface {
+	CreateUser(ctx *gofr.Context, user *models.User) (*models.User, error)
+	GetUser(ctx *gofr.Context, CUID string) (*models.User, error)
+}
