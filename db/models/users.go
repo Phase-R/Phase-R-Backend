47c505e759cb8959791e59c8803126f954fc3074
samyak2003-coder <samyaksinghal2003@gmail.com>
2024-06-29 17:39:15 +0530

    sample

--- db/models/users.go
+++ db/models/users.go
@@ -1,12 +1,12 @@
 package models
 
 type User struct {
-	ID       string `json:"id"`
-	Username string `json:"username"`
-	Fname    string `json:"fname"`
-	Lname    string `json:"lname"`
-	Email    string `json:"email"`
-	Password string `json:"-"`
-	Age      int    `json:"age"`
-	Access   string `json:"access"`
+	ID       string `json:"id" gorm:"primaryKey;column:id"`
+	Username string `json:"username" gorm:"column:username;unique;not null"`
+	Fname    string `json:"fname" gorm:"column:fname;not null"`
+	Lname    string `json:"lname" gorm:"column:lname;not null"`
+	Email    string `json:"email" gorm:"column:email;unique;not null"`
+	Password string `json:"-" gorm:"column:password;not null"`
+	Age      int    `json:"age" gorm:"column:age;not null"`
+	Access   string `json:"access" gorm:"column:access;not null"`
 }
