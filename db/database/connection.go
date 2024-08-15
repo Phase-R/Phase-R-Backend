7d5adacad93f0e96685faa5de5f44f21c80c58ed
Digvijay Narayan <digvijayn1993@gmail.com>
2024-08-15 15:26:14 +0530

    added gym model migrations

--- db/database/connection.go
+++ db/database/connection.go
@@ -27,6 +27,6 @@ func InitDB() *gorm.DB {
 	if err != nil {
 		log.Fatalf("failed to connect database: %v", err)
 	}
-	db.AutoMigrate(&models.User{}, &models.Activities{}, &models.ActivityType{}, &models.Drill{},&models.UserActivityMapping{},&models.ActivityAndActivityTypeMapping{},&models.DrillCompletion{})
+	db.AutoMigrate(&models.User{}, &models.Activities{}, &models.ActivityType{}, &models.Drill{},&models.UserActivityMapping{},&models.ActivityAndActivityTypeMapping{},&models.DrillCompletion{},&models.MuscleGroup{},&models.SubMuscleGroup{},&models.Excercise{})
 	return db
 }
